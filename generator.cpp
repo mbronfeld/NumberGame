feec98ded95fce2026877038ac77a9bef81e9a1e
mbronfeld <msb305@georgetown.edu>
2023-01-23 18:26:57 -0500

    yes

---
+++ generator.cpp
@@ -0,0 +1,78 @@
+// Arguements to this function are the number of numbers followed by those numbers specifically
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char ops[4] = {'a', 's', 'm', 'd'};
+
+inline void getAllOpCombosRec(char set[], string prefix, int n, int k, vector<string>& combos)
+{
+    // Base case: k is 0,
+    // print prefix
+    if (k == 0)
+    {
+        combos.push_back(prefix);
+        return;
+    }
+ 
+    // One by one add all characters
+    // from set and recursively
+    // call for k equals to k-1
+    for (int i = 0; i < n; i++)
+    {
+        string newPrefix;
+         
+        // Next character of input added
+        newPrefix = prefix + set[i];
+         
+        // k is decreased, because
+        // we have added a new character
+        getAllOpCombosRec(set, newPrefix, n, k - 1, combos);
+    }
+}
+
+inline void getAllOpCombos(char set[], int k,int n, vector<string>& combos)
+{
+    getAllOpCombosRec(set, "", n, k, combos);
+}
+
+inline void send(vector<char> vec, int size, vector<string> combos) { // sends the finished data to the console
+    string s = "";
+    for (int i=0; i<size; i++) {
+        s = s + vec[i] + " ";
+    }
+    for (int j=0; j<combos.size(); j++) {
+            cout << s << combos[j] << endl;
+        }
+}
+
+int main(int argc, char **argv) {
+
+    int size = stoi(argv[1]); // defines the number of numbers in the problem
+    
+    // Checks to see if the size passed matches with the number of numbers passed
+    if (argc-2 != size) {
+        cerr << "This program requires " << size << " numbers to run corrctly, only " << argc - 2 << " were passed" << endl;
+        return -1;
+    }
+    
+    vector<char> nums; // initalizes the vector of numbers and assigns them from the arguements
+    vector<string> opCombos; // initializes the array of operations
+    int maxParen = size - 2;
+
+    getAllOpCombos(ops, size - 1, 4, opCombos); // finds all combinations of operations
+    
+    for (int i=0; i<size; i++) {
+        nums.push_back(argv[i+2][0]);
+    }
+
+    do{
+        send(nums, size, opCombos);
+    } while(next_permutation(nums.begin(), nums.end()));
+
+    return 0;
+}
+
+// dont forget to use ./gen 4 1 2 3 4 | xargs -L1 -P0 ./a.out
---
+++ partition.cpp
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+inline bool isInOrder(vector<char> ops, int opsSize) {
+    auto lastSeen = ops.at(0);
+    for (int i=1; i<opsSize; i++) {
+        //cout << "Looking at: " << ops.at(i) << endl;
+        char curr = ops.at(i);
+        if ((curr == '*') || (curr == '/')) {
+            if ((lastSeen == '+') || (lastSeen == '-')) {
+                return false;
+            }
+        }
+        lastSeen = curr;
+    }
+    return true;
+}
+
+inline bool isBetweenNums(int start, int end, int test) {
+    if ((start <= test) && (end >= test)) {
+        return true;
+    }
+    return false;
+}
+
+inline bool isBetweenOps(int start, int end, int test) {
+    if ((start <= test) && (end-1 >= test)) {
+        return true;
+    }
+    return false;
+}
+
+inline void solver(vector<int> nums, char ops[], int numsSize, int opsSize, int start, int end, char* name) {
+    
+    //Creates a vector of specific numbers and operations
+    int chosenOpsSize = end-start;
+    int chosenNumsSize = (end-start)+1;
+    
+    vector<int> chosenNums;
+    for (int i=start; i<=end; i++) {
+        if (isBetweenNums(start, end, i)) {
+            chosenNums.push_back(nums[i]);
+        }
+    }
+    vector<char> chosenOps;
+    for (int i=start; i<end; i++) {
+        if (isBetweenOps(start, end, i)) {
+            chosenOps.push_back(ops[i]);
+        }
+    }
+    if (!isInOrder(chosenOps, chosenOpsSize)) { // Checks to see if the operations are in priority order
+        //cout << "ending early" << endl;
+        //cout << "end" << endl;
+        return;
+    }
+    
+    // Performs the chosen mathematical operations on the chosen numbers
+    int soFar = 0;
+    for (int i=0; i<chosenOpsSize; i++) {
+        int a;
+        int b;
+        int result;
+        if (i==0) {
+            a = chosenNums[0];
+        }
+        else {
+            a = soFar;
+        }
+        b = chosenNums[i+1];
+        char op = chosenOps[i];
+        //cout << "a: " << a << " b: " << b << " op: " << op << endl;
+        switch (op) {
+            case 'a':
+                result = a + b;
+                break;
+            case 's':
+                result = a - b;
+                break;
+            case 'm':
+                result = a * b;
+                break;
+            case 'd':
+                if (b!=0) {
+                    if (a%b == 0) {
+                    result = a/b;
+                    }
+                    else {
+                        //cout << "end" << endl;
+                        return;
+                    }
+                }
+                else {
+                    //cout << "ending early" << endl;
+                    //cout << "end" << endl;
+                    return;
+                }
+                break;
+        }
+        soFar = result;
+    }
+    
+    int chosenNumPicker = 0;
+    int unchosenNumPicker = 0;
+    int chosenOpPicker = 0;
+    int unchosenOpPicker = 0;
+    string s = ""; 
+    //string s = name;
+    //s = s + " ";
+    int i = 0;
+    while (i<numsSize) {
+        int toBeAdded;
+        if (isBetweenNums(start, end, i)) {
+            toBeAdded = soFar;
+            i = i + chosenNumsSize - 1;
+        }
+        else {
+            toBeAdded = nums.at(i);
+        }
+        s = s + to_string(toBeAdded);
+        s = s + " ";
+        i++;
+    }
+    i=0;
+    while (i<opsSize) {
+        char toBeAdded;
+        if (isBetweenOps(start, end, i)) {
+            i = i + chosenOpsSize;
+        }
+        toBeAdded = ops[i];
+        unchosenOpPicker ++;
+        s = s + toBeAdded;
+        i++;
+    }
+    
+    cout << s << endl;
+    return;
+}
+
+int main(int argc, char **argv) {
+
+    if (argc <= 2){ // base case of no further operations
+        cout << argv[1] << endl;
+        return 0;
+    }
+    else { // recussive element
+        int size = argc - 2;
+
+        // creates an array of characters that represent the operations
+        string opString = argv[argc-1];
+        char ops[size-1];
+        strcpy(ops, opString.c_str());
+
+        // creates a vector of the numbers represented as integers
+        vector<int> nums;
+        for (int i=1; i<=size; i++) {
+            nums.push_back(stoi(argv[i]));
+        }     
+
+        for (int start=0; start<size-1; start++) {
+            for (int end=start+1; end<size; end++) {
+                solver(nums, ops, size, size-1, start, end, argv[0]);
+            }
+        }
+    }
+    return 0;
+}
+
+//./gen 4 1 2 3 4 | xargs -L1 -P0 ./part | xargs -L1 -P0 ./part | xargs -L1 -P0 ./part
