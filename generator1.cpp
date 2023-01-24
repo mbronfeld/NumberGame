d162bb825993395e8ef515dde45d16372b6ecd92
mbronfeld <msb305@georgetown.edu>
2023-01-24 03:14:34 -0500

    fghn

--- generator1.cpp
+++ generator1.cpp
@@ -3,15 +3,53 @@
 
 using namespace std;
 
-/*inline string getAllOpCombosRec(char set[], string prefix, int n, int k)
+
+void getCombinations(std::vector<char> &arr, std::vector<char> &data, int start, int end, int index, int n)
+{
+    if (index == n)
+    {
+        string s = "";
+        s = s + to_string(n);
+        for (int i = 0; i < n; i++){
+            s = s + " ";
+            s = s + data[i];
+        }
+        cout << s << endl;
+        return;
+    }
+    for (int i = start; i <= end; i++)
+    {
+        data[index] = arr[i];
+        getCombinations(arr, data, i, end, index+1, n);
+    }
+}
+
+void _getCombinations(std::vector<char> &arr, std::vector<char> &data, int start, int end, int index, int n){
+    getCombinations(arr, data, 0, arr.size()-1, 0, n);
+}
+
+int main(int argc, char **argv)
+{
+    char numbers[sizeof(char) * 10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    vector<char> arr;
+    for (int i = 0; i <= 9; i++)
+        arr.push_back(numbers[i]);
+    int n = stoi(argv[1]); // change n to the desired combination length
+    vector<char> data(n);
+    _getCombinations(arr, data, 0, arr.size()-1, 0, n);
+    return 0;
+}
+
+
+/*
+inline void getAllNumCombosRec(char set[], string prefix, int n, int k, int size)
 {
     // Base case: k is 0,
     // print prefix
     if (k == 0)
     {
-        //cout << prefix << endl;
-        //return;
-        return prefix;
+        cout << size << prefix << endl;
+        return;
     }
  
     // One by one add all characters
@@ -22,27 +60,26 @@ using namespace std;
         string newPrefix;
          
         // Next character of input added
-        newPrefix = prefix + set[i];
+        newPrefix = prefix + " ";
+        newPrefix = newPrefix + set[i];
          
         // k is decreased, because
         // we have added a new character
-        getAllOpCombosRec(set, newPrefix, n, k - 1);
+        getAllNumCombosRec(set, newPrefix, n, k - 1, size);
     }
 }
 
-inline void getAllOpCombos(char set[], int k,int n)
+inline void getAllNumCombos(char set[], int k,int n)
 {
-    getAllOpCombosRec(set, "", n, k);
-}*/
+    getAllNumCombosRec(set, "", n, k, k);
+}
 
 int main(int argc, char **argv) {
     
     int size = stoi(argv[1]);
-    char numbers[sizeof(char) * 10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    char numbers[sizeof(char) * 10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
     
-    
-    //getAllOpCombos(nuumbers, size, 10)
-    
-    cout << "4 5 4 2 3" << endl;
+    getAllNumCombos(numbers, size, 10);
 
-}
+
+}*/
--- script.sh
+++ script.sh
@@ -2,17 +2,18 @@
 
 size=$1
 
-echo "Size: $size"
+#echo "Size: $size"
 
-for (( i=1 ; i<=$size ; i++ ));
+for (( i=1 ; i<=$size+1 ; i++ ));
 do
     rm results$((i)).txt 2> /dev/null
 done
 
-./start $1 | xargs -L1 ./gen > results1.txt
+./start $1 | xargs -L1 -P0 ./gen > results1.txt
 
 for (( i=1 ; i<=$size ; i++ ));
 do
     echo $i
-    cat results${i}.txt | xargs -L1 -P0 ./part > results$((i+1)).txt
+    time (cat results${i}.txt | xargs -L1 -P0 ./part | uniq > results$((i+1)).txt 2> /dev/null)
+
 done
