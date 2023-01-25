d4c2ec3174159677bdce3cdd6d4a3fa9177d9c5e
mbronfeld <msb305@georgetown.edu>
2023-01-25 14:20:26 -0500

    sort -u

--- givenN.sh
+++ givenN.sh
@@ -8,9 +8,10 @@ rm results*.txt 2>/dev/null
 
 for (( i=1 ; i<=$size ; i++ ));
 do
-    echo $i
-    time (cat results${i}.txt | uniq | xargs -L1 -P0 ./part  > results$((i+1)).txt 2> /dev/null)
+    echo $'\n'$i
+    time (cat results${i}.txt | sort -u | xargs -L1 -P0 ./part  > results$((i+1)).txt 2> /dev/null)
 done
 
+echo $'\n'
 #cat results${size}.txt | sort -n | uniq -c | sort -k 1n
 cat results${size}.txt | sort -nu
