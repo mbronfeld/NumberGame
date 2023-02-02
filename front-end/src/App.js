7ae411cdefcd72307ff933d0ded28d61324e8fa1
mbronfeld <msb305@georgetown.edu>
2023-02-02 00:24:58 -0500

    please work

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,10 +6,10 @@ import ChosenButtonArray from './ChosenButtonArray';
 import UndoButton from './UndoButton';
 
 let buttons = [
-  {text: 7, id: "1", visible: true},
-  {text: 7, id: "2", visible: true},
-  {text: 3, id: "3", visible: true},
-  {text: 3, id: "4", visible: true}
+  {text: 1, id: 1, visible: true},
+  {text: 2, id: 2, visible: true},
+  {text: 3, id: 3, visible: true},
+  {text: 4, id: 4, visible: true}
 ];
 
 let lastButtons = [
@@ -62,25 +62,38 @@ export default function App() {
           result = parseInt(result)
         }
     }
-    buttons.push({text: result, 
-                  id: biggestID + 1, 
-                  visible: true});
-
-    setBiggestID(biggestID + 1)
+    pushLastState(bottomNumberButtons)
+    const unusedButtons = bottomNumberButtons.filter((button) => button.id !== topLeftNumberButton.id && button.id !== topRightNumberButton.id)
+    unusedButtons.push({text: result, id: topLeftNumberButton.id , visible: true})
+    setBottomNumberButtons(unusedButtons)
     checkWinCondition()
     setTopLeftNumberButton(null)
     setTopRightNumberButton(null)
     setTopOpButton(null)
-    }
+  }
+  
 
-    const undo = () => {
-      console.log('UNDOING')
-      const newBottomButtons = buttons.filter((button) => (button.id !== biggestID));
-      setBottomNumberButtons(newBottomButtons)
-    }
+  const undo = () => {
+    console.log('UNDOING')
+    const temp = copyButtons(lastState[lastState.length - 1])
+    //
+    setBottomNumberButtons(temp)
+    setLastState(lastState.slice(0, lastState.length - 1))
+    console.log(lastState)
+  }
 
+  const copyButtons = (buttons, forceVisible=false) => {
+    let newButtons = []
+    //console.log(buttons)
+    buttons.forEach((elem) => {
+      const visible = forceVisible || elem.visible
+      const toBeAdded = {text: elem.text, id: elem.id, visible: visible}
+      newButtons = [...newButtons, toBeAdded]
+    });
+    return newButtons
+  }
+  
   const [winReached, setWinReached] = React.useState(false)
-  const [biggestID, setBiggestID] = React.useState(buttons.length);
   const [bottomNumberButtons, setBottomNumberButtons] = React.useState(buttons);
   const [topLeftNumberButton, setTopLeftNumberButton] = React.useState();
   const [topRightNumberButton, setTopRightNumberButton] = React.useState();
@@ -88,7 +101,17 @@ export default function App() {
   const [bottomOpButtons, setBottomOpButton] = React.useState(operations);
   const [topOpButton, setTopOpButton] = React.useState();
 
-  const [lastBottomNumberButtons, setLastBottomNumberButtons] = React.useState([]);
+  const [lastState, setLastState] = React.useState([]);
+
+  const [addState, setAddState] = React.useState(false);
+
+  const pushLastState = React.useCallback((bottomNumberButtons) => {
+      console.log("nbnb : " , bottomNumberButtons)
+      const temp = copyButtons(bottomNumberButtons, true)
+      setLastState([...lastState, temp])
+      console.log("new last state: " , lastState)
+      setAddState(false)
+    }, [lastState]);
 
   const handleBottomNumberClick = React.useCallback((buttonID) => {
       if ((topLeftNumberButton) && (topRightNumberButton)) {
@@ -139,7 +162,7 @@ export default function App() {
           Try to reach: {target}
       </div>
       <div className='undo-button-container'>
-        <UndoButton onClick={() => undo()}/>
+        {lastState.length > 0 ? <UndoButton onClick={() => undo()}/> : null}
       </div>
       {!winReached ? <header className="App-header">
          <ChosenButtonArray numberButtons={[topLeftNumberButton, topRightNumberButton]}
@@ -157,4 +180,4 @@ export default function App() {
 }
 
 
-/* instead of filetering out buttons set visible oand not visible on bottom buttons */
+//max shi is a genius
